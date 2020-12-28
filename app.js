11a66ea93d12d553565909d8c98dc02f846fd216
42azimut <azimutuniverse@gmail.com>
2020-12-28 13:57:34 +0900

    ejs install error in win10

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const app = express();
 
+//app 세팅
+app.set('views', '/views');
+// ejs 엔젠으로 뷰를 해석
+app.engine('html', require('ejs').renderFile);
+//app.set('views engine', 'ejs');
+
+
+
 app.get('/', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="ko">  
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignUp</title>
-  </head>
-  <body>
-    여기는 루트 입니다.~~
-    <p><a href="/login"> login 하시용 </a></p>
-  </body>
-  </html>
-  `);
+  res.render('/home/index');
 });
 
 app.get('/login', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="ko">  
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignUp</title>
-  </head>
-  <body>
-    <input type="text" placeholder="아이디"><br>
-    <input type="text" placeholder="비밀번호"><br>
-    <button>로그인</button>
-  </body>
-  </html>
-  `);
+  res.render(`login`);
 });
 
 app.listen(3000, () => {
