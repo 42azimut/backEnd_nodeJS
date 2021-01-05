6baa655fd390603f4f7447eb8b49be47567aa1b5
42azimut <azimutuniverse@gmail.com>
2021-01-06 00:16:17 +0900

    app.listen module

--- routes/home/home.ctrl.js
+++ routes/home/home.ctrl.js
@@ -13,20 +13,3 @@ module.exports = {
     home,
     login,
 };
-=======
-"use strict";
-
-const home = (req, res) => {
-  res.render('home/index');
-};
-
-const login = (req, res) => {
-  res.render(`home/login`);
-};
-
-// object 형태로 모듈.넘김 키==값 같으면 값 생략 가능
-module.exports = {
-  home,
-  login,
-} 
->>>>>>> 81edc993ed56a6393a9ec3f1c4946ea4ad8c823d
--- routes/home/index.js
+++ routes/home/index.js
@@ -4,11 +4,6 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./home.ctrl');
 
-<<<<<<< HEAD
-const ctrl = require("./home.ctrl");
-
-=======
->>>>>>> 81edc993ed56a6393a9ec3f1c4946ea4ad8c823d
 router.get('/', ctrl.home);
 router.get('/login', ctrl.login);
 
