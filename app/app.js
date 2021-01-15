e74582c80e5b0b836d7caecf79755435df2babfc
42azimut <azimutuniverse@gmail.com>
2021-01-15 20:41:11 +0900

    front js folder

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs'); // ejs 엔진으로 뷰를 해석
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);  // use >>> 미들웨어middelware 등록해주는 메서드
 
