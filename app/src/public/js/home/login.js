13b8559abf8717fe54a23e3f9ad68742c3eb8e7d
42azimut <azimutuniverse@gmail.com>
2021-01-17 00:23:21 +0900

    DOM |  object 제어하기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello js public dir");
-console.log("bye again");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+console.log(id);
+console.log("hi there jayden");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    /* fetch(); 다음 시간에 */
+}
