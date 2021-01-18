f29ba2fe34f172af3409851825a912378f522cb7
42azimut <azimutuniverse@gmail.com>
2021-01-18 19:46:25 +0900

    User Model done

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,33 +1,34 @@
 "use strict";
 
 class UserStorage {
-    static #users = {
-        id: ["jay", "dust", "mydev"],
-        psword: ["123", "1234", "12345"],
-        name: ["보랏스", "태지", "또람프"]
-    };
+  static #users = {
+    id: ["jay", "dust", "nada"],
+    psword: ["123", "1234", "12345"],
+    name: ["토람프", "아이유", "비티엣"],
+  };
 
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-               newUsers[field] = users[field]; 
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
+  static getUsers(...fields) {
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
 
-    static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users);  // [id, psword, name]
-        const userInfo = userKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-        return userInfo;
-    }
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // => [id, psword, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
 }
 
-module.exports = UserStorage
+module.exports = UserStorage;
